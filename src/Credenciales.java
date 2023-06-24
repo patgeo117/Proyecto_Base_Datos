7051b2c3ed69ea3135687a590caa954f5050b222
patgeo117 <gpatino017@gmail.com>
2023-06-24 17:24:15 -0500

    Base Inicial 1.1

--- src/Credenciales.java
+++ src/Credenciales.java
@@ -1,5 +1,5 @@
 public class Credenciales {
-    static String archivoSQL = "src/Consultas.sql"; // se toma la ruta de las consultas
+    //static String archivoSQL = "src/Consultas.sql"; // se toma la ruta de las consultas
     static String user = "George117"; // datos para el login
     static String password = "Pinina117";
     static String url = "jdbc:postgresql://localhost:5432/FundacionProNiñez"; // URL para conectar a la base de datos
